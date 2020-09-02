ce6c4e64233f7431e97cee02911072582bd8ffc5
Jan-Andreas Rusnak <j4n4ndre4sr@gmail.com>
2020-09-02 19:02:02 +0200

    third commit

--- src/main/java/YatzyGame.java
+++ src/main/java/YatzyGame.java
@@ -1,5 +1,11 @@
 public class YatzyGame {
     public int score(YatzyCategory category, int[] dice) {
-        return 4;
+        int result = 0;
+        for (int die : dice) {
+            if (die == 1) {
+                result += 1;
+            }
+        }
+        return result;
     }
 }
--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -11,4 +11,10 @@ public class YatzyTest {
         assertEquals(4, yatzyGame.score(YatzyCategory.ONES, new int[] { 1, 1, 2, 1, 1}));
         assertEquals(2, yatzyGame.score(YatzyCategory.ONES, new int[] { 6, 5, 2, 1, 1}));
     }
+
+    @Test
+    void shouldScoreOnes() {
+        assertEquals(4, yatzyGame.score(YatzyCategory.TWOS, new int[] { 1, 1, 2, 1, 1}));
+        assertEquals(2, yatzyGame.score(YatzyCategory.TWOS, new int[] { 6, 5, 2, 1, 1}));
+    }
 }
