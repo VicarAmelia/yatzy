1d7ff9a5be08b9d66f41b558bbf441624da5bbec
Jan-Andreas Rusnak <42783523+VicarAmelia@users.noreply.github.com>
2020-09-02 20:19:15 +0200

    Implement PAIR and THREE OF A KIND (#1)
    * pair test
    
    * new test pairs
    
    * progress
    
    * three of a kind added
    
    * fix three of a kind

--- src/main/java/YatzyCategory.java
+++ src/main/java/YatzyCategory.java
@@ -1,3 +1,3 @@
 public enum YatzyCategory {
-    TWOS, ONES
+    TWOS, PAIR, ONES, THREE_OF_A_KIND;
 }
--- src/main/java/YatzyGame.java
+++ src/main/java/YatzyGame.java
@@ -2,10 +2,28 @@ public class YatzyGame {
     public int score(YatzyCategory category, int[] dice) {
         int result = 0;
         for (int die : dice) {
-            if (die == 1) {
+            if (die == 1 && category == YatzyCategory.ONES) {
                 result += die;
             } else if (die == 2 && category == YatzyCategory.TWOS) {
                 result += die;
+            }
+        }
+        int[] frequencies = new int[7];
+        for (int die : dice) {
+            frequencies[die]++;
+        }
+        if (category == YatzyCategory.PAIR) {
+            for (int value = frequencies.length - 1; value >= 0; value--) {
+                if (frequencies[value] == 2) {
+                    return value * 2;
+                }
+            }
+        }
+        if (category == YatzyCategory.THREE_OF_A_KIND) {
+            for (int value = frequencies.length - 1; value >= 0; value--) {
+                if (frequencies[value] == 3) {
+                    return value * 3;
+                }
             }
         }
         return result;
